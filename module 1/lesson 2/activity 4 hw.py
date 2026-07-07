5f6ead3017aaebe3683787a0dc9e29e61a46d7d4
safrinsamarah <shakhayat1511@gmail.com>
2026-07-07 19:45:20 +0600

    lesson 2 activity 4 hw

---
+++ module 1/lesson 2/activity 4 hw.py
@@ -0,0 +1,12 @@
+Adilla = "1 june"
+Sumanah = "12 November"
+Niladri = "6 june"
+loba = "2 November"
+Sabiya = "15 September"
+
+print("🍰 STORING BIRTHDAYS🎂")
+print(f"Adilla birthday is on {Adilla}")
+print(f"Sumanah birthday is on {Sumanah}")
+print(f"Niladri birthday is on {Niladri}")
+print(f"loba birthday is on {loba}")
+print(f"Sabiya birthday is on {Sabiya}")
