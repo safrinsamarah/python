81e74c42d13d08dce562a65e96cd6345b908b781
safrinsamarah <shakhayat1511@gmail.com>
2026-07-12 18:23:32 +0600

    lesson 3 activity 1

---
+++ module 1/lesson 3/activity 1.py
@@ -0,0 +1,17 @@
+a = 5 #data type -> integer
+x = 2.345 #data type -> float
+isStudent = True #data type -> boolean
+y = "27" #data type -> str
+list1 = [1, 2, 3, 4, 5] #data type -> list
+dict1 = {"Name":"Safrin", "Age":14, "Grade":8} #data type -> dictionary
+
+#Types of values
+print(type(x))
+print(type(y))
+
+#Typecasting -> Converting one data type to another data type
+x = int(x)
+print(x, type(x))
+
+y = float(y)
+print(y, type(y))
