e71299a4213d5dc5be4e2fc082a014621ba112b5
safrinsamarah <shakhayat1511@gmail.com>
2026-07-12 18:40:46 +0600

    lesson 3 activity 4

---
+++ module 1/lesson 3/activity 4.py
@@ -0,0 +1,24 @@
+text1 = "Python"
+text2 = "Programming"
+#Slicing
+print(text1[0:3]) #it will be index+1
+
+#Reverse
+print(text1[::-1])
+
+#Concat
+print(text1+ " " +text2)
+
+#length
+print(len(text2))
+
+#Upper
+print(text1.upper())
+
+#Lower
+print(text1.lower())
+
+#Find
+print(text1.find("th"))
+
+
