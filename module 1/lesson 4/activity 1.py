2e1037563a42f93b190f2e028e042bd72b53573d
safrinsamarah <shakhayat1511@gmail.com>
2026-07-13 19:23:28 +0600

    lesson 4 activity 1

---
+++ module 1/lesson 4/activity 1.py
@@ -0,0 +1,13 @@
+Math = 98
+Science = 96
+Bangla = 81
+English = 99
+ICT = 88
+BGS = 80
+
+total = Math+Science+Bangla+English+ICT+BGS
+print(f"Total number is {total}")
+
+avg = total/6
+
+print(f"Average number is {round(avg, 2)}")
