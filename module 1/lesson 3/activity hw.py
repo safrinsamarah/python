6d89cf542ade07a41825304fc66931bb2c3f339d
safrinsamarah <shakhayat1511@gmail.com>
2026-07-13 17:30:26 +0600

    lesson 3 activity hw

---
+++ module 1/lesson 3/activity hw.py
@@ -0,0 +1,12 @@
+name = input("Enter your friend's name: ")
+age = int(input("Enter your friend's age: "))
+hobbies = input("Enter your friend's hobbies (separate by commas): ")
+
+# Reverse
+reversed_name = name[::-1]
+
+# Display the results
+print("Age:", age)
+print("Reversed Name:", reversed_name)
+# upper
+print(hobbies.upper())
