276390d28ed13f9d3089bc9433684bc52c27b9b1
safrinsamarah <shakhayat1511@gmail.com>
2026-07-20 18:42:48 +0600

    lesson 5 activity hw

---
+++ module 1/lesson 5/activity hw.py
@@ -0,0 +1,11 @@
+temperature = float(input("Enter the current temperature (10 - 100°C): "))
+if temperature == 10:
+    print("Wear a heavy jacket, sweater, scarf, gloves, and boots.")
+elif temperature == 20:
+    print("Wear a light jacket or hoodie with jeans.")
+elif temperature == 30:
+    print("Wear a T-shirt with jeans or two piece.")
+elif temperature == 40:
+    print("Wear light cotton clothes such as a T-shirt and shorts or light or air pass dress.")
+else:
+    print("INVALID INPUT")
