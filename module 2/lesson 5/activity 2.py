3023314ac0b702fce38c00971b4849998be449f4
safrinsamarah <shakhayat1511@gmail.com>
2026-08-24 19:33:09 +0600

    lesson 5 activity 2

---
+++ module 2/lesson 5/activity 2.py
@@ -0,0 +1,17 @@
+start_day = 2 # 0=sun, 1=mon, 2=tues
+total_days = 30
+
+column = 0
+
+#Print blank padding before day 1
+for i in range(start_day):
+    print(" ",end="")
+    column += 1
+
+#Print the actual dates
+for day in range(1, total_days + 1):
+    print(f"days:3",end="")
+    column = 1
+    if column == 7:
+        print()
+        column = 0
