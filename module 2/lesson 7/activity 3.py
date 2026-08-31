c0ac39b6c918837a1c59c9d3bc15d3f9d1112b0d
safrinsamarah <shakhayat1511@gmail.com>
2026-08-31 19:44:11 +0600

    lesson 7 activity 3

---
+++ module 2/lesson 7/activity 3.py
@@ -0,0 +1,10 @@
+choice = int(input("Enter your choice (1-3): "))
+
+if choice == 1:
+    print("Starting game...")
+elif choice == 2:
+    pass
+elif choice == 3:
+    print("Exciting...")
+else:
+    Print("Invalid choice")
