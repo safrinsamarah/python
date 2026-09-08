f146bbb765aa76f0518fb3794bf8c260a84213d4
safrinsamarah <shakhayat1511@gmail.com>
2026-09-08 21:50:24 +0600

    lesson 1 activity 3

---
+++ module 3/lesson 1/activity 3.py
@@ -0,0 +1,9 @@
+def rectangle(l,w):
+    area = l*w 
+    return area
+print(f"Area of rectangle {rectangle(20,10)}")
+
+def circle(radius):
+    area =3.14*radius*radius
+    return circle
+print(f"Area of circle {circle(20)}")
