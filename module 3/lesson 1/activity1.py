2ebf6e76cf30f76611a15f26938585cb568cbdb0
safrinsamarah <shakhayat1511@gmail.com>
2026-09-08 21:14:22 +0600

    lesson 1 activity 1

---
+++ module 3/lesson 1/activity1.py
@@ -0,0 +1,10 @@
+def greet_user(name):
+    print(f"Hello {name}! Welcome to Python")
+
+def goodbye(name):
+    print(f"Bye {name}! See you next time.")
+
+
+name = input("Enter your name: ")
+greet_user(name)
+goodbye(name)
