13c3b377adbef33ed93e23d8d4690e1436baef3b
safrinsamarah <shakhayat1511@gmail.com>
2026-09-14 17:20:40 +0600

    lesson 2 activity 1

---
+++ module 3/lesson 1/activity.py
@@ -0,0 +1,5 @@
+i = 1
+while i <= 2:
+    for j in range(1, 4):
+        print(i, j)
+        i += 1
---
+++ module 3/lesson 2/activity 1.py
@@ -0,0 +1,7 @@
+def show_profile(name,hobby):
+    print(f"Hello {name}, nice to meet you. You have a nice hobby which is {hobby}")
+
+name = input("What is your name: ")
+hobby = input("what is your hobby: ")
+
+show_profile(name,hobby)
